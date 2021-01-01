6d27fb225e50dabdd5e47177f0cbb785180f09ae
sourabhsh55 <sourabhsh55@gmail.com>
2021-01-02 02:27:33 +0530

    corrected addProduct inside deleteProduct route

---
+++ backend/app/routes/carts.route.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cartRouter = express.Router();
+const controller = require("../controllers/cart.controller");
+const {isAuth} = require("../middlewares/auth.verify");
+const{
+    verifyAllProductField,
+    verifyAddProductField,
+    verifyDeleteProductField
+} = require("../middlewares/cartField.verify");
+
+// allProducts
+cartRouter.post("/allProducts",verifyAllProductField,isAuth,controller.allProducts);
+// cartRouter.get("/allProducts",controller.allProducts);
+
+// addProduct
+cartRouter.post("/addProduct",verifyAddProductField,isAuth,controller.addProduct);
+// cartRouter.post("/addProduct",controller.addProduct);
+
+// DeleteProduct
+cartRouter.delete("/deleteProduct",verifyDeleteProductField,isAuth,controller.deleteProduct);
+
+// Export Module
+module.exports = cartRouter;
