18ccf54b454b8d53ef76bfb24d2efa5394a4105a
Sourabh Sharma <43003495+sourabhsh55@users.noreply.github.com>
2021-01-03 17:58:41 +0530

    Update order.Model.js

---
+++ backend/app/models/orderModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    orders : [
+        {
+            address:{type:String,required:true},
+            pincode:{type:Number,required:true},
+            isDelivered:{type:Boolean,default:false},
+            totalCost:{type:Number,required:true},
+            items_id : {type:Array,required:true},
+            Qty:{type:Array,required:true},
+        }
+    ]
+
+})
+
+const Orders = mongoose.model('Orders',orderSchema);
+
+exports.Orders = Orders;
