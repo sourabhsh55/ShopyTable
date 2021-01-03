8b304953e2ed2b5b71b05431830ceedc10a83daa
sourabhsh55 <sourabhsh55@gmail.com>
2021-01-03 17:46:32 +0530

    all endPoints updated

--- backend/app/routes/auth.route.js
+++ backend/app/routes/auth.route.js
@@ -1,82 +1,23 @@
 const express = require("express");
 const authRoute = express.Router();
-const {Users} = require("../models/userModel");
-const {generateToken} = require("../utils");
-const bcrypt = require("bcrypt");
-
-authRoute.use(express.json());
+const controller = require("../controllers/auth.controller.js");
+const {
+    verifyRegister_LoginField,
+    verifynewAccessTokenField,
+    verifyLogoutField
+} = require("../middlewares/authField.verify");
 
 // REGISTER
-authRoute.post("/register",async (req,res)=>{
-
-    const Username = req.body.username;
-    const Password = req.body.password;
-    const Email = req.body.email;
-
-
-    const isRegistered = await Users.findOne({email:Email});
-    if(isRegistered){
-        res.status(422).send("User already exists!");
-        return;
-    }
-
-    try{
-        const salt = await bcrypt.genSalt();
-        const hashed_password = await bcrypt.hash(Password,salt);
-
-        const new_user = new Users({
-            username:Username,
-            email:Email,
-            password:hashed_password
-        });
-
-        const reg_user = await new_user.save();
-        res.status(201).json({"registered user":reg_user});
-        return;
-    }
-    catch{
-        res.status(500).send("~ERROR : 00AUTH/register");
-        return;
-    }
-});
+authRoute.post("/register",verifyRegister_LoginField,controller.register);
 
 // LOGIN
-authRoute.post("/login",async (req,res)=>{
-    const Username = req.body.username;
-    const Password = req.body.password;
-    const Email = req.body.email;
+authRoute.post("/login",verifyRegister_LoginField,controller.login);
 
+// NewAccessToken
+authRoute.post("/newAccessToken",verifynewAccessTokenField,controller.newAccessToken);
 
-    const isRegistered = await Users.findOne({email:Email});
+// Logout
+authRoute.delete("/logout",verifyLogoutField,controller.logout);
 
-    if(!isRegistered){
-        res.status(401).send("User doesn't exists!");
-        return;
-    }
-
-    try{
-        const correctPassword = isRegistered.password;
-        const isPasswordCorrect = await bcrypt.compare(Password,correctPassword);
-
-        if(!isPasswordCorrect){
-            res.status(401).send("you have entered a wrong password!");
-            return;
-        }
-
-        const user_payLoad = {
-            email:Email,
-            username:Username
-        };
-
-        const token = generateToken(user_payLoad);
-        res.status(201).json({token:token});
-        return;
-    }
-    catch{
-        res.send("~ERROR : 00AUTH/login");
-        return;
-    }
-})
-
-
+//Export module
 module.exports = authRoute;
--- backend/app/routes/orders.route.js
+++ backend/app/routes/orders.route.js
@@ -1,2 +1,17 @@
+const express = require("express");
+const orderRouter = express.Router();
+const controller = require("../controllers/order.controller");
+const { isAuth } = require("../middlewares/auth.verify");
+const { verifyAddOrder } = require("../middlewares/orderField.verify");
 
-// api/user/orders(get)
+// allOrders
+orderRouter.post("/allOrders",isAuth,controller.allOrders);
+
+// placeOrder
+orderRouter.post("/placeOrder",verifyAddOrder,isAuth,controller.placeOrder);
+
+// updateOrder
+orderRouter.post("/updateOrder",isAuth,controller.updateOrder);
+
+// Export Module
+module.exports = orderRouter;
