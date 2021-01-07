cd11239cefc009688fc75dceea095993d6482603
Sourabh Sharma <43003495+sourabhsh55@users.noreply.github.com>
2021-01-08 00:16:35 +0530

    CORS added

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,16 @@ const cartsRoute = require("./app/routes/carts.route");
 const authRoute = require("./app/routes/auth.route");
 
 app.use(express.json());
-app.use(cors());
+app.use(cors());        
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(function(req,res,next){
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+})
 
 app.use("/products",productRoute);
 app.use("/orders",ordersRoute);
