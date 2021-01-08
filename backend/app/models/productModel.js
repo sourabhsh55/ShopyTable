d6601e93717e4141a362fda7176a7df736d806db
sourabhsh55 <sourabhsh55@gmail.com>
2021-01-09 03:17:39 +0530

    band name is compulsory

--- backend/app/models/productModel.js
+++ backend/app/models/productModel.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
     name:{type:String,required:true},
     category:{type:String,required:true},
-    brand:{type:String},
+    brand:{type:String,required:true},
     price:{type:Number,required:true},
     countInStock:{type:Number,required:true},
     rating:{type:Number,required:true},
+    image:{type:String,required:true}
 });
 
 const Products = mongoose.model('Products',productSchema);
