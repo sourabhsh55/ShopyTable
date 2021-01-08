e0ebd7ba8bff1ace36f2f52b7403945d1df6844b
sourabhsh55 <sourabhsh55@gmail.com>
2021-01-09 03:10:28 +0530

    after_render and other changes

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,41 @@
-import {getProductID} from "../api.js";
+import {addCartItem, getProductID} from "../api.js";
 import Rating from "../components/Rating.js";
 import parseURL from "../utils.js";
 
 
 const ProductScreen = {
+
+    after_render : async()=>{
+        const add_btn = document.getElementById("add-button");
+        if(add_btn){
+            add_btn.addEventListener("click",async()=>{
+                const select_ele = document.getElementsByClassName("qty-select")[0];
+                const product_Qty = select_ele.value;
+                const productID = select_ele.id;
+                if(localStorage.getItem('Access_Token') && localStorage.getItem('Username')){
+                    await addCartItem({product_Qty,productID});
+                    document.location.hash = '/cart';
+                    return;
+                }
+                else{
+                    alert('you need to login first');
+                    document.location.hash = '/';
+                    return;
+                }
+            })
+        }
+        
+    },
+
     render:async()=>{
         const request = parseURL();
         const id = request.id;
-        const product = await getProductID(id);
+        var product = await getProductID(id);
+
+        if(product.error){
+            return `<div><h1>${product.error}</h1></div>`
+        }
+        product = product.product;
         return `
 
             <div class="content">
@@ -38,6 +66,16 @@ const ProductScreen = {
                         ${product.description}
                     </div>
                     </li>
+                    <li>
+                    Qty: 
+                    <select class="qty-select" id="${product._id}">
+                    ${[...Array(product.countInStock).keys()].map((x) =>
+                        product.qty === x + 1
+                        ? `<option selected value="${x + 1}">${x + 1}</option>`
+                        : `<option  value="${x + 1}">${x + 1}</option>`
+                    )}
+                    </select>
+                    </li>
                 </ul>
                 </div>
                 <div class="details-action">
@@ -54,7 +92,12 @@ const ProductScreen = {
                         }
                     </li>
                     <li>
-                        <button id="add-button" class="fw primary">Add to Cart </div>
+                        ${
+                            product.countInStock > 0
+                            ? `<button id="add-button" class="fw primary">Add to Cart </div>`
+                            : `<button id="add-button" class="fw" disabled>Add to Cart </div>`
+                        }
+                    </li>
                     </ul>
                 </div>
             </div>
