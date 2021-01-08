41af72916efcad93cf5eda50da3c5e5ee69e656c
sourabhsh55 <sourabhsh55@gmail.com>
2021-01-09 03:05:04 +0530

    headers set

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,19 @@ const ordersRoute = require("./app/routes/orders.route");
 const cartsRoute = require("./app/routes/carts.route");
 const authRoute = require("./app/routes/auth.route");
 
+require('dotenv').config();
+
 app.use(express.json());
-app.use(cors());
+app.use(cors());        
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(function(req,res,next){
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+})
 
 app.use("/products",productRoute);
 app.use("/orders",ordersRoute);
@@ -19,7 +29,7 @@ app.use("/auth",authRoute);
 
 
 //----------------------------------------------------------------------------
-mongoose.connect('mongodb://127.0.0.1/ShopyTable',{ useNewUrlParser: true,useUnifiedTopology: true })
+mongoose.connect(`${process.env.MONGODB}/ShopyTable`,{ useNewUrlParser: true,useUnifiedTopology: true })
  .then(()=>console.log('connected to mongodb'))
  .catch((err)=>console.error('could not connect, ERROR:',err));
 
