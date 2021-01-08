3139559bb0471e25c621bf8937ff70eec59bb575
sourabhsh55 <sourabhsh55@gmail.com>
2021-01-09 03:11:35 +0530

    hash change after register

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,4 +1,5 @@
 import { getRegistered } from "../api.js";
+import { getUser } from "../localStorage.js";
 
 
 const RegisterScreen = {
@@ -7,18 +8,24 @@ const RegisterScreen = {
         const submit_btn = document.getElementById('submit-btn');
 
         submit_btn.addEventListener('click',async(e)=>{
-            e.preventDefault();
+            // e.preventDefault();
 
-            const name = document.getElementById('name');
-            const email = document.getElementById('email');
-            const password = document.getElementById('password');
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
 
             const info = {
                 username:name,
                 email:email,
                 password:password
             };
-            getRegistered(info);
+            const obj = await getRegistered(info);
+            if(obj.error){
+                alert(obj.error);
+            }
+            else{
+                document.location.hash = "/signin";
+            }
         })
     },
 
@@ -26,6 +33,11 @@ const RegisterScreen = {
 
         // check if localStorage is having the token or not
         //  --> IF YES, then redirect towards the homepage.
+        const name = localStorage.getItem("Username");
+        const token = localStorage.getItem("Access_Token");
+        if(name && token){
+            document.location.hash = '/singin';
+        }
 
         return `
             <div class="form-container">
@@ -34,31 +46,34 @@ const RegisterScreen = {
                 <li>
                     <h1>Create Account</h1>
                 </li>
-                <li>
-                    <label for="name">Name</label>
-                    <input type="name" name="name" id="name" />
-                </li>
-                <li>
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" />
-                </li>
-                <li>
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" />
-                </li>
-                <li>
-                    <label for="repassword">Re-Enter Password</label>
-                    <input type="password" name="repassword" id="repassword" />
-                </li>
-                <li>
-                    <button type="submit" id="submit-btn" class="primary">Register</button>
-                </li>
-                <li>
-                    <div>
-                    Already have an account?
-                    <a href="/#/signin">Sign-In </a>
-                    </div>
-                </li>
+                
+                    <li>
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" required/>
+                    </li>
+                    <li>
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" required/>
+                    </li>
+                    <li>
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" required/>
+                    </li>
+                    <li>
+                        <label for="repassword">Re-Enter Password</label>
+                        <input type="password" name="repassword" id="repassword" required/>
+                    </li>
+                    <li>
+                        <button type="submit" id="submit-btn" class="primary">Register</button>
+                    </li>
+                
+                    <li>
+                        <div>
+                        Already have an account?
+                        <a href="/#/signin">Sign-In </a>
+                        </div>
+                    </li>
+                
                 </ul>
             </form>
             </div>
