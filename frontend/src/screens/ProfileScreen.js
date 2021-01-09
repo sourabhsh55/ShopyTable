b9061eb280ae04a5dc695e1ad96739e5c5aa4484
sourabhsh55 <sourabhsh55@gmail.com>
2021-01-09 19:02:28 +0530

    added refresh_token request

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,37 +1,61 @@
-import { updateOrder,getAllOrders } from "../api.js";
+import { updateOrder,getAllOrders, checkIsAdmin, getNewAccessToken } from "../api.js";
 import { delUser } from "../localStorage.js";
 
 const ProfileScreen = {
 
     after_render : async()=>{
         const logout_btn = document.getElementById('signout-button');
-        logout_btn.addEventListener('click',()=>{
-            delUser();
-            document.location.hash = "/";
-        })
-
-        const yes_btns = document.getElementsByClassName("yes-button");
-        for(let i=0;i<yes_btns.length;i++){
-            yes_btns[i].addEventListener('click',async()=>{
-                let r = confirm("Are you sure you have received your package?");
-                if(r==true){
-                    await updateOrder(yes_btns[i].id);
-                    document.getElementsByClassName(yes_btns[i].id)[0].childNodes[9].innerText = true;
-                    yes_btns[i].disabled = true;
-                    yes_btns[i].innerText = "Done";
-                }
+        if(logout_btn){
+            logout_btn.addEventListener('click',()=>{
+                delUser();
+                document.location.hash = "/";
             })
         }
+        
 
-        const add_product_btn = document.getElementById("Add-product-button");
-        add_product_btn.addEventListener('click',()=>{
-            document.location.hash = '/add-product';
-        })
+        const yes_btns = document.getElementsByClassName("yes-button");
+        if(yes_btns){
+            for(let i=0;i<yes_btns.length;i++){
+                yes_btns[i].addEventListener('click',async()=>{
+                    let r = confirm("Are you sure you have received your package?");
+                    if(r==true){
+                        await updateOrder(yes_btns[i].id);
+                        document.getElementsByClassName(yes_btns[i].id)[0].childNodes[9].innerText = true;
+                        yes_btns[i].disabled = true;
+                        yes_btns[i].innerText = "Done";
+                    }
+                })
+            }
+
+            const add_product_btn = document.getElementById("Add-product-button");
+            if(add_product_btn){
+                add_product_btn.addEventListener('click',()=>{
+                    document.location.hash = '/add-product';
+                })
+            }
+        }
     },
 
     render : async()=>{
 
-        const {ORDERS:orders} = await getAllOrders();
+        if(!localStorage.getItem("Username")){
+            document.location.hash = '/';
+        }
+        var obj_orderItem = await getAllOrders();
+
+        // if token is expired and make a request at the backend for the new Access_Token
+        if(obj_orderItem.error == "jwt expired" || obj_orderItem.error == "invalid token" || obj_orderItem.error == "invalid signature"){
+            // alert("jwt expired");
+            const newToken = await getNewAccessToken();
+            if(newToken.error){
+                // alert(newToken.error);
+                document.location.hash = '/';
+                return;
+            }
+            localStorage.setItem("Access_Token",newToken.token);
+            obj_orderItem = await getAllOrders();
+        }
+        const {ORDERS:orders} = obj_orderItem;
         console.log(orders.length);
         const name = localStorage.getItem("Username");
         const email = localStorage.getItem("Email");
